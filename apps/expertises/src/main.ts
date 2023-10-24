f08636f8ffe4fb4575fca6880d9b552f1ed8b4df
cinelly <admin@cinelly.com>
2023-10-24 14:01:08 +0200

    expertises

--- apps/expertises/src/main.ts
+++ apps/expertises/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
       queue: 'expertises',
     },
   });
-  console.log('hallcjffffo')
+  console.log('hallcjffffo');
   app.useLogger(app.get(Logger));
   await app.startAllMicroservices();
   await app.listen(configService.get('PORT'));
